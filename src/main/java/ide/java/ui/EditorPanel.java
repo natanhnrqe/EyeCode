486e39016384c6353d40de92ffde3790e0fbaebb
Natan <natanhenriqueramossilva@gmail.com>
2026-04-03 21:53:54 -0300

    feat: implement editor panel

--- src/main/java/ide/java/ui/EditorPanel.java
+++ src/main/java/ide/java/ui/EditorPanel.java
@@ -1,4 +1,26 @@
 package ide.java.ui;
 
-public class EditorPanel {
+import javax.swing.*;
+import java.awt.*;
+
+public class EditorPanel extends JPanel {
+
+    private JTextArea textArea;
+
+    public EditorPanel() {
+
+        setLayout(new BorderLayout());
+
+        textArea = new JTextArea();
+        textArea.setFont(new Font("Consolas", Font.PLAIN, 14));
+
+        JScrollPane scrollPane = new JScrollPane(textArea);
+
+        add(scrollPane, BorderLayout.CENTER);
+
+    }
+
+    public JTextArea getTextArea() {
+        return textArea;
+    }
 }
