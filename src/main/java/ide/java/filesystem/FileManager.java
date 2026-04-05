261fde6d20ab13638dbd1fc0720c1dca3d9471d2
Natan <natanhenriqueramossilva@gmail.com>
2026-04-04 21:22:33 -0300

    feat: implement file manager for opening and saving file

---
+++ src/main/java/ide/java/filesystem/FileManager.java
@@ -0,0 +1,30 @@
+package ide.java.filesystem;
+
+import java.io.*;
+
+public class FileManager {
+
+    public String openFile(File file){
+        StringBuilder content = new StringBuilder();
+
+        try(BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line;
+
+            while ((line = reader.readLine()) != null){
+                content.append(line).append("\n");
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    return content.toString();
+
+    }
+    public void saveFile(File file, String content){
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+            writer.write(content);
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
