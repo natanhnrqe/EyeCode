33cefba2d2c8e7527502361aee97b25ff4f90298
Natan <natanhenriqueramossilva@gmail.com>
2026-04-04 22:25:29 -0300

    feat: add document model

---
+++ src/main/java/ide/java/editor/Document.java
@@ -0,0 +1,41 @@
+package ide.java.editor;
+
+import java.io.File;
+
+public class Document {
+
+    private File file;
+    private String content;
+    private Boolean modified;
+
+    public Document(File file, String content) {
+        this.file = file;
+        this.content = content;
+        this.modified = false;
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+        this.modified = true;
+    }
+
+    public Boolean getModified() {
+        return modified;
+    }
+
+    public void setModified(Boolean modified) {
+        this.modified = modified;
+    }
+}
