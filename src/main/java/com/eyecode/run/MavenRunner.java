2ae7f982bcccf7c668b01cbd99a29533510c10cd
Natan <natanhenriqueramossilva@gmail.com>
2026-06-05 22:01:44 -0300

    feat(maven): add MavenRunner support

---
+++ src/main/java/com/eyecode/run/MavenRunner.java
@@ -0,0 +1,47 @@
+package com.eyecode.run;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+
+public class MavenRunner implements ProjectRunner{
+    @Override
+    public String run(File projectRoot) {
+
+        StringBuilder output = new StringBuilder();
+
+        try {
+
+            ProcessBuilder builder = new ProcessBuilder(
+                    "mvn",
+                    "clean",
+                    "compile"
+            );
+
+            builder.directory(projectRoot);
+
+            Process process = builder.start();
+
+            BufferedReader reader =
+                    new BufferedReader(
+                            new InputStreamReader(
+                                    process.getInputStream()
+                            )
+                    );
+
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+
+                output.append(line).append("\n");
+            }
+            process.waitFor();
+
+        }catch (Exception e) {
+
+            output.append(e.getMessage());
+        }
+
+        return output.toString();
+    }
+}
